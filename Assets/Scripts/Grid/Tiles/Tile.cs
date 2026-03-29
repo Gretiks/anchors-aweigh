731c08ea49a11bad0deb86b7e333567b6be69bd1
Mateusz Bagiński <m.baginski@o2.pl>
2026-03-29 13:21:58 +0200

    Improved grid manager, added player's ship tiles

--- Assets/Scripts/Grid/Tiles/Tile.cs
+++ Assets/Scripts/Grid/Tiles/Tile.cs
@@ -4,8 +4,19 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private GameObject _highlight;
 
     public void Innit(bool isOffset) { 
         _spriteRenderer.color = isOffset ? _offsetColor : _baseColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        _highlight.SetActive(false);
     }
 }
--- Assets/Scripts/Grid/gridManager.cs
+++ Assets/Scripts/Grid/gridManager.cs
@@ -3,9 +3,8 @@
 public class gridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
-    
-    [SerializeField] private Tile _tilePrefab;
 
+    [SerializeField] private Tile _shipTile, _seaTile;
     [SerializeField] private Transform _camera;
 
     private void Start()
@@ -19,15 +18,48 @@ public class gridManager : MonoBehaviour
         {
             for (int y = 0; y < _height; y++)
             {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                bool isShip = IsShipTile(x, y);
+                var spawnedTile = Instantiate(isShip ? _shipTile : _seaTile, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
-
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Innit(isOffset);
             }
         }
-
         _camera.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
     }
-    
+
+    bool IsShipTile(int x, int y)
+    {
+        float centerX = Mathf.Round((_width - 1) / 4f);
+        float centerY = (_height - 1) / 2f;
+
+        // Fixed ship dimensions in tiles
+        int hullHalfWidth = 4;
+        int sternHalfWidth = 3;
+        int bowHeight = 4;
+        int hullHeight = 8;
+        int sternHeight = 1;
+
+        int shipTotalHeight = bowHeight + hullHeight + sternHeight;
+
+        int shipStartY = Mathf.RoundToInt(centerY - shipTotalHeight / 2f);
+        int bowStartY = shipStartY + sternHeight + hullHeight;
+
+        if (y < shipStartY || y >= shipStartY + shipTotalHeight) return false;
+
+        if (y >= bowStartY)
+        {
+            float t = (float)(y - bowStartY) / bowHeight;
+            float halfWidth = Mathf.Lerp(hullHalfWidth, 0f, t);
+            return Mathf.Abs(x - centerX) < halfWidth + 0.5f;
+        }
+        else if (y >= shipStartY + sternHeight)
+        {
+            return Mathf.Abs(x - centerX) <= hullHalfWidth;
+        }
+        else
+        {
+            return Mathf.Abs(x - centerX) <= sternHalfWidth;
+        }
+    }
 }
