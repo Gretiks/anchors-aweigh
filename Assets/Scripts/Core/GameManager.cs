8e2e5472b4e4be7657d9a0e1cbc19d59ae58a24d
Gretiks <156473423+Gretiks@users.noreply.github.com>
2026-05-19 13:23:01 +0200

    Enemy AI

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using Grid;
+using System.Collections;
 using Assets.Scripts.Grid.Tiles.Modules;
 
 namespace Core
@@ -43,9 +44,8 @@ namespace Core
                     ResetCannonsFired();
                     break;
                 case GameState.EnemyTurn:
-                    ProcessHelmMovement();
-                    CheckBattleConditions();
-                    ChangeState(GameState.UserTurn);
+                    ResetEnemyMovment();
+                    StartCoroutine(EnemyTurnRoutine());
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
@@ -57,11 +57,55 @@ namespace Core
             ChangeState(GameState.EnemyTurn);
         }
 
+        private IEnumerator EnemyTurnRoutine()
+        {
+            Debug.Log("-----TURA AI-----");
+            
+            BaseEnemy.AssignEnemiesToModules();
+            
+            foreach (var enemy in UnitManager.Instance._enemies)
+            {
+                if (enemy == null) continue;
+
+                enemy.StartTurnAction();
+                
+                while (!enemy.isActionCompleted)
+                {
+                    yield return null;
+                }
+            }
+            
+            ProcessEnemyCannons();
+            
+            ProcessHelmMovement();
+            CheckBattleConditions();
+            
+            ChangeState(GameState.UserTurn);
+        }
+
+        private void ProcessEnemyCannons()
+        {
+            var cannons = FindObjectsByType<CannonTile>();
+            foreach (var cannon in cannons)
+            {
+                if (cannon.owner == Faction.Enemy)
+                {
+                    cannon.EnemyExecuteFire();
+                }
+            }
+        }
+        
         private void ResetHeroMovement()
         {
             foreach (var hero in UnitManager.Instance._heroes)
                 hero.ResetMovement();
             MenuManager.Instance.RefreshHeroList(UnitManager.Instance._heroes);
+        }
+
+        private void ResetEnemyMovment()
+        {
+            foreach (var enemy in UnitManager.Instance._enemies)
+                enemy.ResetMovement();
         }
 
         private void ResetCannonsFired()
@@ -92,7 +136,7 @@ namespace Core
             if (playerPos <= -10f || enemyPos >= 10f)
                 Debug.Log("Ucieczka!");
             else if (Mathf.Abs(playerPos - enemyPos) < 1f)
-                Debug.Log("Aborda¿!");
+                Debug.Log("Abordaz!");
         }
     }
 
